0ee30af97ea811516670a4c82d1683d192401ada
BlooketHacks.au <107653282+jHAX7@users.noreply.github.com>
2022-06-18 11:58:56 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.log('This cheat was made in javascript (and html in some hacks) view how to use them in a README.md')
+console.log('These cheats was made in javascript (and html in some hacks) view how to use them in a README.md')
 
 //file made for indicator that its made in javascript.
